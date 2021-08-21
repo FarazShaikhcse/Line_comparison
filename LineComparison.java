997a7bea678922208f77ba85401b91418ce1d19f
Faraz Shabbir <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-08-21 17:38:33 +0530

    added equality uc2

--- LineComparison.java
+++ LineComparison.java
@@ -4,15 +4,26 @@ import java.lang.Math;
 public class LineComparison{
 	public static void main(String args[]){
 		System.out.println("Welcome to Line Comparison Computation Program");
-		System.out.println("Enter the line co-ordinates");
-		int x1,y1,x2,y2;
-    		double dist;
+		int x11,y11,x12,y12,x21,y21,x22,y22;
+    		double dist1,dist2;
 		Scanner input=new Scanner(System.in);
-		x1=input.nextInt();
-		y1=input.nextInt();
-		x2=input.nextInt();
-		y2=input.nextInt();
-		dist=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		System.out.println("The length of the line is:"+String.format("%.4f",dist));		
+		System.out.println("Enter the co-ordinates of line-1");
+		x11=input.nextInt();
+		y11=input.nextInt();
+		x12=input.nextInt();
+		y12=input.nextInt();
+		dist1=Math.sqrt((x12-x11)*(x12-x11)+(y12-y11)*(y12-y11));
+		System.out.println("Enter the co-ordinates of line-2");
+		x21=input.nextInt();
+                y21=input.nextInt();
+                x22=input.nextInt();
+                y22=input.nextInt();
+                dist2=Math.sqrt((x22-x21)*(x22-x21)+(y22-y21)*(y22-y21));
+		System.out.println("The length of the line1 is:"+String.format("%.4f",dist1));
+		System.out.println("The length of the line2 is:"+String.format("%.4f",dist2));		
+		if(dist1==dist2)
+			System.out.println("The lines are equal");
+		else
+			System.out.println("The lines are not equal");
 }
 }
