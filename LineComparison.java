8b3a0226ef023f452044f2d986f43bcf7637dbe7
Faraz Shabbir <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-08-21 17:13:50 +0530

    added length calculation

--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,18 @@
+import java.util.*;
+import java.lang.Math;
+
 public class LineComparison{
 	public static void main(String args[]){
 		System.out.println("Welcome to Line Comparison Computation Program");
+		System.out.println("Enter the line co-ordinates");
+		int x1,y1,x2,y2;
+    		double dist;
+		Scanner input=new Scanner(System.in);
+		x1=input.nextInt();
+		y1=input.nextInt();
+		x2=input.nextInt();
+		y2=input.nextInt();
+		dist=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println("The length of the line is:"+String.format("%.4f",dist));		
 }
 }
